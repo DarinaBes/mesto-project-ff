2e6c8dba463e7f701751a6399c075517340128f9
Darina Besliney <darinabes92@mail.ru>
2024-07-26 14:57:51 +0300

    задание сделано

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
       name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      link: "https://images.unsplash.com/photo-1708814438006-365af488eb14?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fCVEMCVCMCVEMSU4MCVEMSU4NSVEMSU4QiVEMCVCN3xlbnwwfHwwfHx8MA%3D%3D",
     },
     {
       name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      link: "https://images.unsplash.com/photo-1721308000535-429e1d8de107?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      link: "https://images.unsplash.com/photo-1721806689685-b4ff76322c1a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      link: "https://images.unsplash.com/photo-1645128187991-3030aa113532?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fCVEMCVCQSVEMCVCMCVEMCVCQyVEMSU4NyVEMCVCMCVEMSU4MiVEMCVCQSVEMCVCMHxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
       name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      link: "https://images.unsplash.com/photo-1721774542595-75f5ae9da106?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      link: "https://images.unsplash.com/photo-1614000531402-74cca389903f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8JUQwJUIxJUQwJUIwJUQwJUI5JUQwJUJBJUQwJUIwJUQwJUJCfGVufDB8fDB8fHww",
     }
 ];
